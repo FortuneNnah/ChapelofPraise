7cafd141c8ca05c2251aebedeb333f9fc96fc7b4
Fortune Nnah <Fortunennah747@gmail.com>
2026-04-17 00:34:36 +0100

    Day 2

--- src/Components/Herosection.jsx
+++ src/Components/Herosection.jsx
@@ -21,8 +21,14 @@ export const Header = () => {
 
 export const Hero = () => {
   return(
-  <div className="Hero-container">
-
+  <div className="Hero-section">
+    <div className="Hero-container">
+      <h1>Welcome home to <br />Chapel Of Praise</h1>
+      <p>No matter where you are on your journey, you are always welcome here.</p>
+      <p>Whether you're looking for answers or a place to belong, we invite you to join our us.</p>
+    </div>
+      {/* <button id="herobtn" c
+      lassName="herobtn">Join Us</button> */}
   </div>
 )};
 
